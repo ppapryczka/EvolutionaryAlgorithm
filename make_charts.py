af9f048de66a7724e6a92156dc7d5dd001a2ee59
Patryk Pankiewicz <patryk.pankiewicz369@gmail.com>
2019-01-09 09:51:00 +0100

    Python changes.

--- make_charts.py
+++ make_charts.py
@@ -5,9 +5,26 @@ import matplotlib.pylab as plt
 
 def read_data(fileName):
     data = pd.read_csv(fileName, delimiter=",", header = None)
-    print(data)
     return data
 
+
+def make_ECDF(fileName_List):
+    minValues = np.arange(len(fileName_List))
+
+    for x in range(len(fileName_List)):
+        data = read_data(fileName_List[x])
+        minElement = data[0].min()
+        minValues[x] = minElement
+
+    x = np.sort(minValues)
+    y = np.arange(1, len(minValues) + 1) / len(minValues)
+
+    plt.plot(x, y, marker='.', linestyle='none')
+    plt.title('ECDF')
+    plt.grid(True)
+    fig = plt.gcf()
+    fig.savefig("QualityECDF.png")
+    plt.close(fig)
 
 def make_charts(fileName):
     data = read_data(fileName)
@@ -17,29 +34,38 @@ def make_charts(fileName):
     plt.plot(x_axis, data[0])
     plt.title('Min value')
     plt.grid(True)
-    plt.show()
+    fig = plt.gcf()
+    fig.savefig(fileName.split(".csv")[0]+"MinValue.png")
+    plt.close(fig)
 
     plt.plot(x_axis, data[1])
     plt.title('Median')
     plt.grid(True)
-    plt.show()
+    fig = plt.gcf()
+    fig.savefig(fileName.split(".csv")[0] + "Median.png")
+    plt.close(fig)
 
     plt.plot(x_axis, data[2])
     plt.title('Average')
     plt.grid(True)
-    plt.show()
+    fig = plt.gcf()
+    fig.savefig(fileName.split(".csv")[0] + "Average.png")
+    plt.close(fig)
 
     plt.plot(x_axis, data[3])
     plt.title('Standard deviation')
     plt.grid(True)
-    plt.show()
+    fig = plt.gcf()
+    fig.savefig(fileName.split(".csv")[0] + "StandardDeviation.png")
+    plt.close(fig)
 
     x = np.sort(data[0])
-
     y = np.arange(1, len(x) + 1) / len(x)
 
     plt.plot(x, y, marker='.', linestyle='none')
-
-    plt.show()
-
+    plt.title('ECDF')
+    plt.grid(True)
+    fig = plt.gcf()
+    fig.savefig(fileName.split(".csv")[0] + "ECDF.png")
+    plt.close(fig)
 
--- run_evolutionary_algorithm.py
+++ run_evolutionary_algorithm.py
@@ -1,7 +1,8 @@
-from make_charts import make_charts
+from make_charts import make_charts, make_ECDF
 import subprocess
 
 def run_and_make_charts(list_name):
+    output_file_names = []
     for x in list_name:
         args = x.split()
         popen = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -9,16 +10,39 @@ def run_and_make_charts(list_name):
         output = popen.stdout.read().decode('utf-8')
         file_name = output.split('\n')
         make_charts(file_name[1])
+        output_file_names.append(file_name[1])
+    return output_file_names
 
-
-
+def run(list_name):
+    output_file_names = []
+    for x in list_name:
+        args = x.split()
+        popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+        popen.wait()
+        output = popen.stdout.read().decode('utf-8')
+        file_name = output.split('\n')
+        print(file_name[1])
+        output_file_names.append(file_name[1])
+    return output_file_names
 
 
 if __name__ == "__main__":
     executable_name = "cmake-build-debug/EvolutionaryAlgorithm"
-    list_name = [
-        executable_name + " -1 100 -2 100 -r 1 -m 0.09 -v 2 -u -c 10 -a 30 -b 0.10 -i 200 -f nowy"
-    ]
 
+    list_name = []
 
-    run_and_make_charts(list_name)
+    for x in range(1000):
+        list_name.append(executable_name + " -1 5 -2 10 -r 1 -m 0.02 -v 2 -u -c 10 -s" + str(x) + " -a 10 -b 0.05 -i 20 -f test" + str(x))
+
+        #list_name.append(executable_name + " -1 5 -2 10 -r 1 -m 0.02 -v 2 -u -c 5 -s"+ str(x) + " -a 10 -b 0.05 -i 20 -f test"+ str(x))
+        #list_name.append(executable_name + " -1 5 -2 10 -r 1 -m 0.05 -v 2 -u -c 5 -s" + str(x) + " -a 10 -b 0.2 -i 20 -f test" + str(x))
+
+    #list_name = [
+    #    executable_name + " -1 100 -2 100 -r 1 -m 0.01 -v 2 -u -c 10 -a 30 -b 0.05 -s 1000 -i 1000 -f test1",
+    #    executable_name + " -1 100 -2 100 -r 1 -m 0.05 -v 2 -u -c 10 -a 30 -b 0.15 -s 1000 -i 1000 -f test2"
+    #]
+    #output_file_names = run_and_make_charts(list_name)
+
+    output_file_names = run(list_name)
+
+    make_ECDF(output_file_names)
